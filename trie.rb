2e6406b0d6b1261865ad1485031faa9d3a544276
Jonathan <jonacom@lissismore.com>
2016-02-17 19:31:36 -0700

    polishing suggest

--- trie.rb
+++ trie.rb
@@ -18,22 +18,29 @@ class Trie
     else
       insert(word, n+1, current.children[word[n]]) if n < word.length - 1
     end
+    @count
   end
 
   def load(file)
     temp = File.read(file)
     temp.split("\n").map do |word|
       insert(word)
-    end
+    end.last
   end
+
+  # def check_input(string)
+  #   return "Invalid input" unless string.chars.all? { |char| letters.include?(char) }
+  #
+  # end
 
 
   def suggest(string, n = 0, current = root)
+    return "Invalid input" if string == ""
+
     @suggestions = []
     @word = ""
-    string.chars.each do |char|
-      current.children.keys.include?(char) ?
-      current = current.children[char] : "No suggestions found"
+    string.each_char do |char|
+      current.children.keys.include?(char) ? current = current.children[char] : (return "No suggestions found")
     end
 
     find_suggestions(string, current)
@@ -41,7 +48,7 @@ class Trie
 
   def find_suggestions(string, current = root)
     unless current.children == {}
-      current.children.keys.each do |key|
+      current.children.each_key do |key|
         @word += key
         @suggestions << (string + @word) if current.children[key].end_of_word
         find_suggestions(string, current.children[key])
@@ -50,6 +57,5 @@ class Trie
     end
     @suggestions
   end
-
 
 end
